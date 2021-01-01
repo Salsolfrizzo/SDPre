18caf1159f961ed0cc3d4beeeda74115e6053b42
sal <frizzosal@gmail.com>
2021-01-01 11:41:27 -0600

    objective1

--- Labs/Objective1Lab1.java
+++ Labs/Objective1Lab1.java
@@ -1,6 +1,8 @@
- public class ClassExample.java {
+ public class Objective1Lab1 {
 
- public static void main(String[] args) {
-        //Where we will write our code.
-  }
+  public static void main(String[] args) {
+    System.out.println("These words will be printed.");
+    System.out.println("And so will these words.");
+    System.out.println("And finally these words.");
+    }
 }
