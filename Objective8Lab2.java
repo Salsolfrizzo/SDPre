849a350eb1ff631d252218a3e703d7b14ce81df9
Salsolfrizzo <74797759+Salsolfrizzo@users.noreply.github.com>
2021-01-05 23:52:42 -0600

    Add files via upload

---
+++ Objective8Lab2.java
@@ -0,0 +1,18 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+
+    int c = 20;
+    int sum = 0;
+
+    for(int i = 0; i <= 20; i++){
+
+
+      sum += i;
+
+    }
+
+      System.out.print(sum);
+ }
+
+
+    }
