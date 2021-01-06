9a93b8101756311478dcfd00c57faf5cb083022b
Salsolfrizzo <74797759+Salsolfrizzo@users.noreply.github.com>
2021-01-05 23:18:25 -0600

    Add files via upload

---
+++ Objective7Lab2.java
@@ -0,0 +1,17 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+
+    while(counter <= 20) {
+      System.out.print(counter);
+      counter = counter + 1;
+
+      if(counter == 21) {
+
+ }
+
+
+    }
+  }
+}
---
+++ Objective7Lab4.java
@@ -0,0 +1,16 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+  int sum = 0;
+  int c = 20;
+
+
+     for(int count = 1; count <= c; count++)  {
+       sum = sum + count;
+
+     }
+
+System.out.println(sum);
+
+
+}
+}
