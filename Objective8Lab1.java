9188bafc276d1ac7358cdcb259055dea056c4a97
Salsolfrizzo <74797759+Salsolfrizzo@users.noreply.github.com>
2021-01-05 23:30:14 -0600

    Add files via upload

---
+++ Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int i = 1; i < 11; i++)
+    System.out.println(i);
+
+    for (int l = 10; l > 0; l--)
+    System.out.println(l);
+  }
+}
